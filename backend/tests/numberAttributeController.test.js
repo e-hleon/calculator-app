270822a964ba7639a4dd6c652dbd9bb986edd74e
e-hleon <e.hleon@ms.ugr.es>
2025-01-27 00:42:53 +0100

    Create numberAttributeController.test.js

---
+++ backend/tests/numberAttributeController.test.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const app = require('../server.js');
+
+describe('Number Attribute Controller', () => {
+
+  test('Should return correct prime = false for 9, and sqrt=3', async () => {
+    const response = await request(app)
+      .get('/api/numberAttribute/9');
+
+    expect(response.status).toBe(200);
+    expect(response.body.value).toBe(9);
+    expect(response.body.isPrime).toBe(false);
+    expect(response.body.squareRoot).toBe(3);
+  });
+
+  test('Should return correct prime = true for 2', async () => {
+    const response = await request(app)
+      .get('/api/numberAttribute/2');
+
+    expect(response.status).toBe(200);
+    expect(response.body.value).toBe(2);
+    expect(response.body.isPrime).toBe(true);
+    // sqrt(2) es aprox 1.414..., para simplificar comparamos con toBeCloseTo
+    expect(response.body.squareRoot).toBeCloseTo(1.414, 3);
+  });
+
+  test('Should fail if invalid number', async () => {
+    const response = await request(app)
+      .get('/api/numberAttribute/hello');
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toMatch(/Invalid number/);
+  });
+});
